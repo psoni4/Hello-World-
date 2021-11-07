b57c274c22b5c1ff63a2d85894784fbf60e3ec70
psoni4 <81704624+psoni4@users.noreply.github.com>
2021-11-07 12:17:23 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
 print('hello all')
+print('This is my first program and would be gradually picking up quick')
+def know_my_profile(bio):
+  print('Name: Preetam Narendra Soni,'+
+         'degree: MS, Data Science'+
+         'Batch: Dec,2022'+
+         'Technologies: Python, R, SQL, PowerBi, Tableau, Spark, PostgreSQL')
+  
